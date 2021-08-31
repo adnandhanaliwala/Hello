f4df323e9a38b6423e8f34749e833b9bb9252c85
adnandhanaliwala <89876521+adnandhanaliwala@users.noreply.github.com>
2021-08-31 17:44:26 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***************************************************************
+Hello.java
+***********************************************************
+Adnan Dhanaliwala
+8/24/2021
+CMSC 255 Section C90
+****************************************************************/
+
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("Adnan Dhanaliwala");
+		System.out.println("Hello.java");
+		System.out.println("Displaying Nane, program name, description, date, course number, and section number");
+		System.out.println("8/24/2021");
+		System.out.println("CMSC 255 Section C90");
+		System.out.println();
+	}
+}
